a8fe30b5c06e59faa54c0c378a5b411032d05ba9
Christof Schwarz <christof.schwarz@gmx.at>
2018-06-22 11:13:30 +0000

    Add files via upload

---
+++ SSEEvaluator.cs
@@ -0,0 +1,166 @@
+﻿namespace SSEDemo
+{
+    using Google.Protobuf;
+    using Grpc.Core;
+    using NLog;
+    using Qlik.Sse;
+    #region Usings
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Security;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+    using System.Threading.Tasks;
+    using static Qlik.Sse.Connector;
+
+    #endregion
+
+    public class SseEvaluator : ConnectorBase, IDisposable
+    {
+        #region Logger
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        #endregion
+
+        public SseEvaluator()
+        {
+            ServicePointManager.ServerCertificateValidationCallback += ValidateRemoteCertificate;
+        }
+
+        private static bool ValidateRemoteCertificate(object sender, X509Certificate cert, X509Chain chain,
+                                                      SslPolicyErrors error)
+        {
+            //Check Server Certificate
+            return true;
+        }
+
+        private BundledRows GetResult(string result)
+        {
+            var resultBundle = new BundledRows();
+            var resultRow = new Row();
+            resultRow.Duals.Add(new Dual { StrData = result, NumData = 0 });
+            resultBundle.Rows.Add(resultRow);
+            return resultBundle;
+        }
+
+        private Row GetParameter(IAsyncStreamReader<BundledRows> requestStream)
+        {
+            try
+            {
+                if (requestStream.MoveNext().Result == false)
+                    logger.Debug("The Request has no parameters.");
+
+                return requestStream?.Current?.Rows?.FirstOrDefault() ?? null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private string GetParameterValue(int index, Row row)
+        {
+            try
+            {
+                if (row == null || row?.Duals?.Count == 0 || index >= row?.Duals.Count)
+                {
+                    logger.Warn($"Parameter index {index} not found.");
+                    return null;
+                }
+
+                return row.Duals[index].StrData;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, $"Parameter index {index} not found with exception.");
+                return null;
+            }
+        }
+
+        #region Public functions
+        public override Task<Capabilities> GetCapabilities(Empty request, ServerCallContext context)
+        {
+            try
+            {
+                logger.Info($"GetCapabilities was called");
+
+                return Task.FromResult(new Capabilities
+                {
+                    AllowScript = false,
+                    Functions =
+                    {
+                        new FunctionDefinition
+                        {
+                            FunctionId = 1,
+                            FunctionType = FunctionType.Scalar,
+                            Name = "MYFUNCTION",
+                            Params =
+                            {
+                                new Parameter { Name = "Text", DataType = DataType.String }
+                            },
+                            ReturnType = DataType.String
+                        }
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "GetCapabilities has errors");
+                return null;
+            }
+        }
+
+        public override async Task ExecuteFunction(IAsyncStreamReader<BundledRows> requestStream, IServerStreamWriter<BundledRows> responseStream, ServerCallContext context)
+        {
+            try
+            {
+                logger.Debug("ExecuteFunction was called");
+                var functionRequestHeaderStream = context.RequestHeaders.SingleOrDefault(header => header.Key == "qlik-functionrequestheader-bin");
+                if (functionRequestHeaderStream == null)
+                {
+                    throw new Exception("ExecuteFunction called without Function Request Header in Request Headers.");
+                }
+
+                var functionRequestHeader = new FunctionRequestHeader();
+                functionRequestHeader.MergeFrom(new CodedInputStream(functionRequestHeaderStream.ValueBytes));
+
+                var commonHeader = context.RequestHeaders.ParseIMessageFirstOrDefault<CommonRequestHeader>();
+                logger.Info($"request from user: {commonHeader.UserId} for AppId: {commonHeader.AppId}");
+
+                await context.WriteResponseHeadersAsync(new Metadata { { "qlik-cache", "no-store" } });
+
+                var functionCall = functionRequestHeader.FunctionId;
+                logger.Debug($"Function id: {functionCall}");
+                var row = GetParameter(requestStream);
+                var input = GetParameterValue(0, row);
+
+                string result = String.Empty;
+                switch (functionCall)
+                {
+                    case 1:
+                        result = input;
+                        break;
+                    default:
+                        result = "Unknown Function";
+                        break;
+                }
+
+                logger.Debug($"Result: {result}");
+                await responseStream.WriteAsync(GetResult(result));
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "ExecuteFunction has errors");
+                await responseStream.WriteAsync(GetResult("Error"));
+            }
+            finally
+            {
+                LogManager.Flush();
+            }
+        }
+        #endregion
+
+        public void Dispose() { }
+    }
+}
